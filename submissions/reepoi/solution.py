d758e5f794abc643e9043b11857ee5899a65e047
Taos Transue <spacechips@rocketmail.com>
2023-02-20 12:22:41 -0700

    update solution

--- submissions/reepoi/solution.py
+++ submissions/reepoi/solution.py
@@ -1,6 +1,7 @@
 import csv
-import json
 import numpy as np
+
+import ivcurves.utils as utils
 
 
 def solution():
@@ -10,13 +11,13 @@ def solution():
     parameters = 2 + np.zeros((32, 6))
     header = 'Index,photocurrent,saturation_current,resistance_series,resistance_shunt,n,cells_in_series'.split(',')
 
-    for name in ['case1.csv', 'case2.csv', 'case3.csv']:
-        with open(name, 'w') as file:
+    for name in utils.get_filenames_in_directory(utils.TEST_SETS_DIR):
+        with open(f'{name}.csv', 'w') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerow(header)
             for idx, row in enumerate(parameters):
                 writer.writerow([1 + idx] + list(row))
 
+
 if __name__ == '__main__':
     solution()
-
